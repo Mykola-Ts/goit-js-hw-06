9b279c9ee6eb552b511965916b54fee50d3d6f1b
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2023-06-18 19:57:44 +0300

    All tasks completed

--- js/task-06.js
+++ js/task-06.js
@@ -15,11 +15,13 @@ function handlerValidation(event) {
     if (input.classList.contains("invalid")) {
       input.classList.remove("invalid");
     }
+
     input.classList.add("valid");
   } else {
     if (input.classList.contains("valid")) {
       input.classList.remove("valid");
     }
+
     input.classList.add("invalid");
   }
 }
